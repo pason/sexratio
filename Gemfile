14522108b94bc8f58221c38cf672b087c98d0798
slawomir <roshendal@gmail.com>
2014-05-27 23:07:42 +0200

    bundler, views

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem 'sinatra'
+gem "koala", "~> 1.10.0rc"
+
--- gender.rb
+++ gender.rb
@@ -4,21 +4,47 @@ require "net/http"
 require "uri"
 require "json"
 
-oauth_access_token = "CAACEdEose0cBANAlZCCYV5eZCszQlkROYH31STwbyX8zZAL0lvpExyOZApGYdMT3PYStr6PtE0WuJ2GXLMJ6Jif2eFIZADvq9xISzKU93YxMQ6kEpw25I3e4lrKK20M0P1d1lGNkZA1JCTc5fRIf8ZCjKnZBpDMPPG8bAIKM8ynVtDhAOQ58C3yuRPc22WZAB9PwrZBBBuVp2ilgZDZD"
+oauth_access_token = "CAACEdEose0cBAAeZBIkl3xGWakvdgapSZAw9L1WRonUlHBsh5T6IMdOjSHb7VAfNSOYIzzzFwJDVBRngPkCXBjZCV8glZBQDBL4B87QkwsWDPA3JMwTS1t1JE3COChtmGZBltNltuojrdvaqjEx9YWaLR8rZBXSfyS39VKbnqTHLwBSCVRYrEfRGsGs7URR44OKNUOMJjRNgZDZD"
 
 
 graph = Koala::Facebook::API.new(oauth_access_token)
-gender = {male: 0, female: 0, unrecognized: 0}
+
+
 
 
 get '/' do
+	erb :index
+end
 
-	event_id = 1379868408963536 
+post '/' do
 
-	attending = graph.get_object("#{event_id}/attending/?fields=first_name")
-	
+	gender = {attending: {}, maybe: {}}
+	match = /events\/(\d+)/.match(params[:event][:url])
+
+	if !match.nil?
+
+		event_id = match[1] 
+
+		event_attending = graph.get_object("#{event_id}/attending/?fields=first_name")
+		event_maybe = graph.get_object("#{event_id}/maybe/?fields=first_name")
+		
+		
+		gender[:attending] = genderize_names(event_attending)
+		gender[:maybe] = genderize_names(event_maybe)
+		
+			
+	end
+
+	erb :index, locals: {gender: gender} 
+
+end
+
+
+def genderize_names(people)
+	gender = {male: 0, female: 0, unrecognized: 0, unbucket: []}
+
 	params = ''
-	attending.each_with_index do |user, id| 
+	people.each_with_index do |user, id| 
 		params += "name[#{id}]=#{user['first_name']}&"
 	end
 	params = params.chomp('&')
@@ -39,12 +65,14 @@ get '/' do
 		result.each do |user|
 			gender[:male] += 1 if user["gender"] == "male"
 			gender[:female] += 1 if user["gender"] == "female"
-			gender[:unrecognized] += 1 if user["gender"].nil?
+			if user["gender"].nil?
+				gender[:unrecognized] += 1
+				gender[:unbucket] << user["name"]
+			end 
 		end
 	else
 		puts "ERROR!!!"
 	end
 
-	raise gender.inspect
-
+	return gender
 end
