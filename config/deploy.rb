41565bcb7229c8997e23764ccc0ed3917ede4b16
slawomir <roshendal@gmail.com>
2015-01-02 23:43:52 +0100

    add rbenv to capfilr

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,6 +36,12 @@ set :keep_releases, 5
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, '2.1.5'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
+
 namespace :deploy do
 
   after :restart, :clear_cache do
